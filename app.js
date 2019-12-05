c70ba2b2feada5262cc3cdbfe0af98d246eaa003
Tamara Trotter <tamaratrotter@Tamaras-iMac.local>
2019-12-05 11:47:27 -0500

    saved app.js changes to add to first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+//Import Routes
+const flightsRoute = require("./routes/flights");
+app.use("/flights", flightsRoute);
+
+app.listen(3000);
