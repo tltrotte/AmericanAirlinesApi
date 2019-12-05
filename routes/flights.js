5dda56919bdf57ab8cee1a53d791d3e73ea72573
Tamara Trotter <tamaratrotter@Tamaras-iMac.local>
2019-12-05 13:09:13 -0500

    error message for status 400 when date is incorrect

--- routes/flights.js
+++ routes/flights.js
@@ -2,7 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-	console.log(req.query);
+	const { originCity, destinationCity, departureDate } = req.query;
+	if (!originCity || !destinationCity || !departureDate) {
+		res.status(400).json({
+			errorMessage:
+				"There are if flights for dates in the past. Please select different date. "
+		});
+	} else {
+		res.json({ numberOfFlights: 2 });
+	}
 });
 
 module.exports = router;
